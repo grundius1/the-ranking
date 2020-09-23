8e99393e3ef1e7cb5e79d3c66f604913f8bc113f
grundius1 <borjallapa@gmail.com>
2020-09-23 19:05:18 +0200

    small updates

--- src/controllers/repositories_controller.py
+++ src/controllers/repositories_controller.py
@@ -3,15 +3,25 @@ from src.app import app
 from flask import request
 from src.database import db
 from src.helpers.json_response import asJsonResponse
-from src.students_update import pulsldata,students_to_db,labs_to_db
+from src.students_update import pulsldata,labs_to_db
+import src.repositories_update as repo
 
 
 @app.route("/<lab>/create",endpoint='func3')
 @asJsonResponse
 def get_repositories(lab):
     lab_data = db["labs"].find_one({"name" : lab})
-    if lab_data == None
+    if lab_data == None:
         labs_to_db(pulsldata())
     lab_data = db["labs"].find_one({"name" : lab})
     return lab_data
+    
+
+@app.route("/lab/update",endpoint='func4')
+@asJsonResponse
+def update_repositories():
+    if all == True:
+        repo.repo_updater(all=all)
+    else:
+        repo.repo_updater()
     
